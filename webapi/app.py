70e99e4515778e63cd5a97a5fce0646cc746fa05
Jauhar Arifin <jauhararifin10@gmail.com>
2018-11-12 21:06:17 +0700

    add preprocessing

--- webapi/app.py
+++ webapi/app.py
@@ -1,8 +1,19 @@
 import os
+import requests
 from sklearn.externals import joblib
 from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_url_path='')
+
+def formalizer(string):
+    req = requests.post('http://127.0.0.1:9000/formalizer', json={'string': string})
+    response = req.json()
+    return response['data']
+    
+def stemmer(string):
+    req = requests.post('http://127.0.0.1:9000/stemmer', json={'string': string})
+    response = req.json()
+    return response['data']
 
 aspects = ['produk', 'pengiriman', 'packaging']
 aspect_predictor_file = [os.getenv('APP_{}_ASPECT_MODEL'.format(aspect), '{}_aspect.sav'.format(aspect)) for aspect in aspects]
@@ -16,6 +27,8 @@ def predict():
     query_str = request.args.get('s')
     if query_str is None:
         return jsonify({})
+
+    query_str = stemmer(formalizer(query_str))
     
     result = {}
     for name, m_aspect, m_sentiment in zip(aspects, aspect_model, sentiment_model):
@@ -25,7 +38,7 @@ def predict():
                 sentiment_result = m_sentiment.predict_proba([query_str])
             except:
                 sentiment_result = m_sentiment.predict([query_str])
-            result[name] = float(sentiment_result[0])
+            result[name] = float(sentiment_result[0] * 2 - 1)
 
     return jsonify(result)
 
